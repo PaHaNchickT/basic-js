737c9b874f8700603c2bcfbc20bb57a68c09c471
Pavel <you@example.com>
2022-05-02 20:39:57 +0300

    feat: passed repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,70 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function repeater(str, opt) {
+  let i = 0
+  let out = ''
+  let add = ''
+  let repeatTimes = opt['repeatTimes']
+  let separator = opt['separator']
+  let addition = opt['addition']
+  let additionRepeatTimes = opt['additionRepeatTimes']
+  let additionSeparator = opt['additionSeparator']
+  
+  if (repeatTimes === undefined) {
+    repeatTimes = 1
+  }
+  if (separator === undefined) {
+    separator = '+'
+  }
+  if (addition === undefined) {
+    addition = ''
+    additionSeparator = ''
+  }
+  if (additionRepeatTimes === undefined) {
+    additionRepeatTimes = 1
+  }
+  if (addition !== undefined && additionSeparator === undefined) {
+    additionSeparator = '|'
+  }
+
+  if (str == null) {
+    str = 'null'
+  }
+  // if (typeof str === 'object') {
+  //   addition = 'STRING_OR_DEFAULT'
+  // }
+  if (addition == null) {
+    addition = 'null'
+  }
+  if (typeof str !== 'string') {
+    str = str.toString()
+  }
+  if (typeof addition === 'object') {
+    addition = addition.toString()
+    if (addition == 'objWithSpecificCoercion') {
+      addition = 'STRING_OR_DEFAULT'
+    } else if (addition === '[object Undefined]') {
+      addition = '[object Object]'
+    }
+  }
+
+  let count = 0
+  while (i < additionRepeatTimes) {
+    if (i === additionRepeatTimes - 1) {
+      add = add+addition
+    } else {
+      add = add+addition+additionSeparator
+    }
+    i++
+  }
+
+  i = 0
+  while (i < repeatTimes) {
+    out = out+str+add+separator
+    i++
+  }
+  return out.slice(0, out.length - separator.length)
 }
 
 module.exports = {
