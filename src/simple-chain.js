a06eaddb700a6d23d060473ea126569fbf03cd68
Pavel <you@example.com>
2022-05-03 02:50:35 +0300

    feat: ya vsyo

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -7,21 +7,14 @@ const { NotImplementedError } = require('../extensions/index.js');
 const chainMaker = {
   getLength() {
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
   }
 };
 
