db37666978ee451168e0bf4eebfb46e5e2a63751
Pavel <you@example.com>
2023-12-21 03:57:42 +0300

    feat: implemet vigenere cipher task (21.12.23 03:57)

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -21,13 +21,96 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(mode) {
+    this.mode = mode
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(word, key) {
+    if (word === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let tempKey = key.toUpperCase(),
+      tempWord = word.toUpperCase(),
+      out = '',
+      alp = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$(),./|*-&^% :'.split(''),
+      keyAlp = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''),
+      j = 0
+
+    for (i = 0; i <= word.length; i++) {
+      let a, b
+      if (j > key.length - 1) {
+        j = 0
+      }
+
+      alp.forEach((e, ind) => {
+        if (ind < 26 && (tempWord[i] === e)) {
+          a = ind
+          keyAlp.forEach((el, indKey) => {
+            if (el === tempKey[j]) {
+              b = indKey
+              out = out + keyAlp[(a + b) % 26]
+            }
+          })
+
+        } else if (ind >= 26 && (tempWord[i] === e)) {
+          out = out + e
+          j--
+        }
+      })
+      j++
+    }
+
+    if (this.mode === false) {
+      return out.split('').reverse().join('')
+    } else {
+      return out
+    }
+  }
+  decrypt(word, key) {
+    if (word === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let tempKey = key.toUpperCase(),
+      tempWord = word.toUpperCase(),
+      out = '',
+      alp = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$(),./|*-&^% :'.split(''),
+      keyAlp = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''),
+      j = 0
+
+    for (i = 0; i <= word.length; i++) {
+      let a, b
+      if (j > key.length - 1) {
+        j = 0
+      }
+
+      alp.forEach((e, ind) => {
+        if (ind < 26 && (tempWord[i] === e)) {
+          a = ind
+          keyAlp.forEach((el, indKey) => {
+            if (el === tempKey[j]) {
+              b = indKey
+              if ((26 + a - b) >= 26) {
+                out = out + keyAlp[a - b]
+              } else {
+                out = out + keyAlp[26 + a - b]
+              }
+            }
+          })
+        } else if (ind >= 26 && (tempWord[i] === e)) {
+          out = out + e
+          j--
+        }
+      })
+      j++
+    }
+
+    if (this.mode === false) {
+      return out.split('').reverse().join('')
+    } else {
+      return out
+    }
   }
 }
 
